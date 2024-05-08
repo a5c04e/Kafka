ae875eba65fee69cb92eac24eda1183b47749c9b
wug <768986235@qq.com>
2024-05-09 00:19:54 +0800

    首次提交

---
+++ src/main/java/producer/CustomProducer.java
@@ -0,0 +1,19 @@
+package producer;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.*;
+
+public class CustomProducer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        Producer<String, String> producer = new KafkaProducer<> (props);
+        for(int i=0;i<100;i++) {
+            producer.send(new ProducerRecord<String, String>(
+
+                    )
+            );
+
+        }
+        producer.close();
+    }
+}
