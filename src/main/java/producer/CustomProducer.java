6212e063547b47ef724fba7ed1f1b6383b48a76b
wug <768986235@qq.com>
2024-05-20 16:57:45 +0800

    第四次提交

--- src/main/java/producer/CustomProducer.java
+++ src/main/java/producer/CustomProducer.java
@@ -2,16 +2,14 @@ package producer;
 
 import java.util.Properties;
 import org.apache.kafka.clients.producer.*;
+import java.util.concurrent.ExecutionException;
 
 public class CustomProducer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException,InterruptedException {
         Properties props = new Properties();
         Producer<String, String> producer = new KafkaProducer<> (props);
         for(int i=0;i<100;i++) {
-            producer.send(new ProducerRecord<String, String>("first",
-                    Integer.toString(i), Integer.toString(i)
-                    )
-            );
+            producer.send(new ProducerRecord<String, String>("first", Integer.toString(i), Integer.toString(i))).get();
         }
         producer.close();
     }
