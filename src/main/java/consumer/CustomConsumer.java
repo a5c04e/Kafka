36ab367b4577f0b7430d83f0a9119333e907638c
wug <768986235@qq.com>
2024-05-27 18:23:25 +0800

    第五次提交

--- src/main/java/consumer/CustomConsumer.java
+++ src/main/java/consumer/CustomConsumer.java
@@ -1,7 +1,30 @@
 package consumer;
 
+import java.util.Arrays;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.*;
+
 public class CustomConsumer {
     public static void main(String[] args) {
-        System.out.println("消费者");
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "192.168.10.102:9092");
+        //设置消费者组test
+        props.put("group.id", "test");
+        //开启自动提交
+        props.put("enable.auto.commit", "true");
+        //设置自动提交时间间隔为1秒
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String,  String>  consumer=new KafkaConsumer<> (props);
+        //消费者订阅first主题
+        consumer.subscribe(Arrays.asList("first"));
+        while (true) {
+            ConsumerRecords<String,  String>  records=consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+            }
+        }
     }
 }
--- src/main/java/producer/CustomProducer.java
+++ src/main/java/producer/CustomProducer.java
@@ -1,14 +1,24 @@
 package producer;
 
 import java.util.Properties;
-import org.apache.kafka.clients.producer.*;
 import java.util.concurrent.ExecutionException;
+import org.apache.kafka.clients.producer.*;
 
 public class CustomProducer {
     public static void main(String[] args) throws ExecutionException,InterruptedException {
         Properties props = new Properties();
+        props.put("bootstrap.servers", "192.168.10.101:9092");
+        props.put("acks", "all");
+        props.put("retries", 1);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
         Producer<String, String> producer = new KafkaProducer<> (props);
         for(int i=0;i<100;i++) {
+            //同步发送
             producer.send(new ProducerRecord<String, String>("first", Integer.toString(i), Integer.toString(i))).get();
         }
         producer.close();
